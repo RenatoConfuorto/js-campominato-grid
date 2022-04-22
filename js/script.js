2f0d81267990beaefcb1cac28dc3aa40447b4bdd
RenatoConfuorto <renato.confuorto@outlook.it>
2022-04-22 18:41:50 +0200

    bonus completato

--- js/script.js
+++ js/script.js
@@ -1,23 +1,48 @@
 // ELEMENTI DEL DOM  
 const main = document.getElementById('main');
-const difficulty = document.getElementById('difficulty');
+const difficultyDisplay = document.getElementById('difficulty');
 const actionBtn = document.getElementById('action-btn');
 
-const gridSize = 10;
-const cellsNumber = gridSize * gridSize;
+//dati inseriti dall'utente da raccogliere
+let difficulty;
+let gridSize;
 
 const activeCells = [];
 //generare la griglia al click del bottone
-actionBtn.addEventListener('click', createGrid);
+actionBtn.addEventListener('click', startGame);
+
+function startGame(){
+  //raccogliere i dati dell'utente e creare la griglia
+  difficulty = difficultyDisplay.value;
+  switch(difficulty){
+    case 'easy': 
+      gridSize = 10;
+      break;
+  
+    case 'medium':
+      gridSize = 9;
+      break;
+  
+    case 'hard':
+      gridSize = 7;
+      break;
+  
+  }
+
+  createGrid();
+}
+
 
 function createGrid(){
   //resettare l'elemento
   main.innerHTML = '';
+  main.className = difficulty;
   //creare il div della griglia
   const grid = document.createElement('div');
   grid.classList.add('container');
   grid.setAttribute('id', 'grid');
   //creare le celle
+  const cellsNumber = gridSize * gridSize;
   createCells(grid, cellsNumber);
 
   main.append(grid);
@@ -27,7 +52,7 @@ function createCells(element, number){
   for(let i = 0; i < number; i++){
     const cell = document.createElement('div');
     cell.classList.add('cell');
-    cell.innerHTML = i;
+    cell.innerHTML = i + 1;
 
     cell.addEventListener('click', function(){
       this.classList.add('active');
